66c63bf8dbdc0fb77b4097a63739bf5be3a73800
Luigiendo <luigiendokun@gmail.com>
2021-05-14 20:23:16 -0500

    Examen
    Examen 3 Parcial

---
+++ Assets/Scrips/HealthBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image imgPortrait;
+    public Image imgHPBar;
+
+    private Character character;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GameObject.FindGameObjectWithTag("Player").GetComponent<Character>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        imgHPBar.fillAmount = (float)character.currentHP / (float)character.HP;
+    }
+}
